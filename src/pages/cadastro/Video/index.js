e1e6b1b5285a8d72ba04eae2c384cd19b58d8693
marlon <marlon_veiga@hotmail.com>
2020-07-31 21:31:08 -0300

    Update read-me.

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -70,7 +70,7 @@ function CadastroVideo() {
           suggestions={categoryTitles}
         />
 
-        <Button type="submit">
+        <Button className="ButtonLink" type="submit">
           Cadastrar
         </Button>
       </form>
